1939f978d88d26219730eb9bb8844a4c937740f2
Морера Боррото Александер Арнальдович <aamoreraborroto@gid.ru>
2026-06-09 15:02:25 +0300

    First commit

--- src/main/java/ru/practicum/shareit/booking/dto/BookingCreateRequest.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingCreateRequest.java
@@ -1,12 +1,11 @@
 package ru.practicum.shareit.booking.dto;
 
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Data
 public class BookingCreateRequest {
@@ -15,20 +14,28 @@ public class BookingCreateRequest {
     private Long itemId;
 
     @NotNull(message = "отсутствует дата начала бронирования")
-    @FutureOrPresent(message = "дата начала бронирования не может быть в прошлом")
     private LocalDateTime start;
 
     @NotNull(message = "отсутствует дата окончания бронирования")
-    @Future(message = "дата окончания бронирования должна быть в будущем")
     private LocalDateTime end;
+
+    @AssertTrue(message = "дата начала бронирования не может быть в прошлом")
+    public boolean isStartNotInPast() {
+        return start == null || !start.isBefore(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @AssertTrue(message = "дата окончания бронирования должна быть в будущем")
+    public boolean isEndInFuture() {
+        return end == null || end.isAfter(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+    }
 
     @AssertTrue(message = "Дата начала бронирования не может быть позже даты окончания бронирования")
     public boolean isStartBeforeEnd() {
-        return start.isBefore(end);
+        return start == null || end == null || start.isBefore(end);
     }
 
     @AssertTrue(message = "Дата начала бронирования не должна совпадать с датой окончания бронирования")
     public boolean isStartNotEqualEnd() {
-        return !start.equals(end);
+        return start == null || end == null || !start.equals(end);
     }
 }
