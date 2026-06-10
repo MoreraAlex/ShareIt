99b6612d8a86c201b1a031c325767200d832cd3d
Морера Боррото Александер Арнальдович <aamoreraborroto@gid.ru>
2026-06-10 19:05:57 +0300

    Changes after review

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
