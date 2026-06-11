6cb633cf184615f94464f1a7cbd8ae82d67e4397
Морера Боррото Александер Арнальдович <aamoreraborroto@gid.ru>
2026-06-11 16:07:41 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.ItemRepository;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.pagination.OffsetPageRequest;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
